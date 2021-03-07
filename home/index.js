ded005a5da199ad41b3860c14d791bd5793b93ac
sketchycrypt <sketchygfx@icloud.com>
2021-03-07 13:10:58 +0000

    mobileversion!

---
+++ home/index.js
@@ -0,0 +1,15 @@
+const navSlide = () => {
+    const burger = document.querySelector('.burger')
+    const nav = document.querySelector('.nav-links')
+    const navLinks = document.querySelectorAll('.nav-links li')
+    //toggles the nav because fuck you
+    burger.addEventListener('click', () => {
+        nav.classList.toggle('nav-active');
+    });
+    //animate the links aaaa
+    navLinks.forEach((link, index) => {
+       link.style.animation = `navLinkFade 0.5s ease forwards ${index / 2}s`;
+    });
+};
+
+navSlide();
